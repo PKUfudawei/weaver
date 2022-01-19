701c513dbdbb6b6406e42b05fe299f1999bdab56
Dawei Fu <fudw@pku.edu.cn>
2022-01-19 18:57:45 +0800

    zeus0119

--- utils/data/fileio.py
+++ utils/data/fileio.py
@@ -20,10 +20,11 @@ def _read_hdf5(filepath, branches, load_range=None):
     import tables
     tables.set_blosc_max_threads(4)
     with tables.open_file(filepath) as f:
-        outputs = {k:getattr(f.root, k)[:] for k in branches}
+        outputs = {k: getattr(f.root, k)[:] for k in branches}
     if load_range is not None:
         start = math.trunc(load_range[0] * len(outputs[branches[0]]))
-        stop = max(start + 1, math.trunc(load_range[1] * len(outputs[branches[0]])))
+        stop = max(
+            start + 1, math.trunc(load_range[1] * len(outputs[branches[0]])))
         for k, v in outputs.items():
             outputs[k] = v[start:stop]
     return outputs
@@ -31,27 +32,31 @@ def _read_hdf5(filepath, branches, load_range=None):
 
 def _read_root(filepath, branches, load_range=None, treename=None):
     _branches = branches.copy()
-    if 'JHUGen' not in filepath:
+    if 'JHUGen234' not in filepath:
         hww_branches = []
         for b in branches:
             if b.startswith('label_H_ww'):
                 _branches.remove(b)
                 hww_branches.append(b)
+
     with uproot3.open(filepath) as f:
         if treename is None:
-            treenames = set([k.decode('utf-8').split(';')[0] for k, v in f.allitems() if getattr(v, 'classname', '') == 'TTree'])
+            treenames = set([k.decode('utf-8').split(';')[0]
+                            for k, v in f.allitems() if getattr(v, 'classname', '') == 'TTree'])
             if len(treenames) == 1:
                 treename = treenames.pop()
             else:
-                raise RuntimeError('Need to specify `treename` as more than one trees are found in file %s: %s' % (filepath, str(branches)))
+                raise RuntimeError('Need to specify `treename` as more than one trees are found in file %s: %s' % (
+                    filepath, str(branches)))
         tree = f[treename]
         if load_range is not None:
             start = math.trunc(load_range[0] * tree.numentries)
             stop = max(start + 1, math.trunc(load_range[1] * tree.numentries))
         else:
             start, stop = None, None
-        outputs = tree.arrays(_branches, namedecode='utf-8', entrystart=start, entrystop=stop)
-    if 'JHUGen' not in filepath:
+        outputs = tree.arrays(_branches, namedecode='utf-8',
+                              entrystart=start, entrystop=stop)
+    if 'JHUGen234' not in filepath:
         nent = len(list(outputs.items())[0][1])
         outputs.update({b: np.zeros(nent, dtype=int) for b in hww_branches})
     return outputs
@@ -63,7 +68,8 @@ def _read_awkd(filepath, branches, load_range=None):
         outputs = {k: f[k] for k in branches}
     if load_range is not None:
         start = math.trunc(load_range[0] * len(outputs[branches[0]]))
-        stop = max(start + 1, math.trunc(load_range[1] * len(outputs[branches[0]])))
+        stop = max(
+            start + 1, math.trunc(load_range[1] * len(outputs[branches[0]])))
         for k, v in outputs.items():
             outputs[k] = v[start:stop]
     return outputs
@@ -79,12 +85,14 @@ def _read_files(filelist, branches, load_range=None, show_progressbar=False, **k
     for filepath in filelist:
         ext = os.path.splitext(filepath)[1]
         if ext not in ('.h5', '.root', '.awkd'):
-            raise RuntimeError('File %s of type `%s` is not supported!' % (filepath, ext))
+            raise RuntimeError(
+                'File %s of type `%s` is not supported!' % (filepath, ext))
         try:
             if ext == '.h5':
                 a = _read_hdf5(filepath, branches, load_range=load_range)
             elif ext == '.root':
-                a = _read_root(filepath, branches, load_range=load_range, treename=kwargs.get('treename', None))
+                a = _read_root(filepath, branches, load_range=load_range,
+                               treename=kwargs.get('treename', None))
             elif ext == '.awkd':
                 a = _read_awkd(filepath, branches, load_range=load_range)
         except Exception as e:
@@ -94,9 +102,10 @@ def _read_files(filelist, branches, load_range=None, show_progressbar=False, **k
         if a is not None:
             for name in branches:
                 table[name].append(a[name].astype('float32'))
-    table = {name:_concat(arrs) for name, arrs in table.items()}
+    table = {name: _concat(arrs) for name, arrs in table.items()}
     if len(table[branches[0]]) == 0:
-        raise RuntimeError(f'Zero entries loaded when reading files {filelist} with `load_range`={load_range}.')
+        raise RuntimeError(
+            f'Zero entries loaded when reading files {filelist} with `load_range`={load_range}.')
     return table
 
 
@@ -104,8 +113,10 @@ def _write_root(file, table, treename='Events', compression=-1, step=1048576):
     if compression == -1:
         compression = uproot3.write.compress.LZ4(4)
     with uproot3.recreate(file, compression=compression) as fout:
-        fout[treename] = uproot3.newtree({k:v.dtype for k, v in table.items()})
+        fout[treename] = uproot3.newtree(
+            {k: v.dtype for k, v in table.items()})
         start = 0
         while start < len(list(table.values())[0]) - 1:
-            fout[treename].extend({k:v[start:start + step] for k, v in table.items()})
+            fout[treename].extend({k: v[start:start + step]
+                                  for k, v in table.items()})
             start += step
