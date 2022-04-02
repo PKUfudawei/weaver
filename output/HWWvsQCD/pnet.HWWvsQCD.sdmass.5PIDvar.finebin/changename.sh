8387afe7d2a9f232a594ec0faaead776543dc151
Dawei Fu <fudw@pku.edu.cn>
2022-04-02 21:38:14 +0800

    upload state files

--- output/HWWvsQCD/pnet.HWWvsQCD.sdmass.5PIDvar.finebin/changename.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for a in {0..29}
-do
-    mv pnet.HWWvsQCD.5PIDvar.finebin_epoch-${a}_optimizer.pt pnet.HWWvsQCD.sdmass.5PIDvar.finebin_epoch-${a}_optimizer.pt
-    mv pnet.HWWvsQCD.5PIDvar.finebin_epoch-${a}_state.pt pnet.HWWvsQCD.sdmass.5PIDvar.finebin_epoch-${a}_optimizer.pt
-done
--- output/HWWvsQCD/pnet.HWWvsQCD.sdmass.5PIDvar/changename.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for a in {0..29}
-do
-    mv pnet.HWWvsQCD.sdmass.5lepvar_epoch-${a}_state.pt pnet.HWWvsQCD.sdmass.5PIDvar_epoch-${a}_state.pt
-    mv pnet.HWWvsQCD.sdmass.5lepvar_epoch-${a}_optimizer.pt pnet.HWWvsQCD.sdmass.5PIDvar_epoch-${a}_optimizer.pt
-done
